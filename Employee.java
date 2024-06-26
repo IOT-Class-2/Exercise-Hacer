cdb62ae8696645939d6e14b37432319f98cdbf19
kayahacer <yhacerkaya@gmail.com>
2024-06-26 09:48:44 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,15 +1,20 @@
 package mextDay2;
 
-public class Employee {
-    private String name;
-    protected double salary ;
+public class Employee { // 'public' Bu sınıfın her yerden erişilebilir olduğunu belirtir
+    private String name; // 'private' Bu değişkenin sadece bu sınıf içinden erişilebilir olduğunu belirtir
+    // çalışanın adını tutan metin değişkenidir
+    protected double salary ; // bu değişkene aynı paket ve alt sınıflardan erişilebilir
     private int age;
 
-    public Employee(String name, double salary, int age){
-        this.name = name;
+    public Employee(String name, double salary, int age){ // bu sınıfın bir örneği oluşturulurken çağrılan metottur
+        this.name = name; //"name" parametresini sınıfın 'name' değişkenine atar
         this.salary = salary;
         this.age = age;
     }
+
+    //Getter ve Setter metotları :
+    // Bu metotlar , özel değişkenlerin değerlerini dış dünyaya(başka sınıflara yani) vermek için kullanılır.
+
     public String getName() {
         return this.name;
     }
@@ -19,7 +24,7 @@ public class Employee {
     public double getSalary(){
         return this.salary;
     }
-    public void raiseSalery(){
+    public void raiseSalery(){ //çalışanın maaşını artıran metot
         this.salary = this.salary * 1.2;
     }
 }
--- Engineer.java
+++ Engineer.java
@@ -1,13 +1,21 @@
 package mextDay2;
 
-public class Engineer extends Employee {
+public class Engineer extends Employee { //'public' bu sınıfın her yerden erişilebilir olduğunu belirtir
+    // 'extends' Employee,Engineer classını(sınıfın) Employee sınıfından miras aldığını belirtir
+    // 'Employee' sınıfından miras aldığını belirtir. Yani ' Engineer' sınıfının tüm özelliklerine ve metotlarına sahip olur.
 
-    public Engineer(String name, double salary, int age){
-        super(name, salary, age);
+    public Engineer(String name, double salary, int age){ // bu sınıfın örneğini oluşturulurken çağrılan metottur.
+        super(name, salary, age);// 'Employee' sınıfının yapıcı metotdunu çağırır.'name','salary','age' parametrelerini
+        // 'Employee' sınıfına iletir.
+        // Bu sınıfın yapıcı metodunu çağırır
     }
 
-    public double getAnnualBonus(){
-        return super.salary * .05;
+
+    // Constracture ---> Yapıcı metot
+    public double getAnnualBonus(){ // Engineer in yıllık bonusunu hesaplayan metottur. Ve bu metotdu döndürür.
+        return super.salary * .05; // Bu metot Employee sınıfından alınan maaş/salary değişkenine erişir
+        //Ne ile erişir? super anahtar kelimesi ile erişir.
+        //Metotun genel işlevi ise maaşın %5' ini hesaplar ve döndürür. Bu Engineer 'in yıllık bonusunu belirler.
     }
 
 }
